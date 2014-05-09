374564edb27cfe10604c697c75b9e1cac93d487f
Marcel Klehr <mklehr@gmx.net>
2014-05-09 17:12:21 +0200

    Start implementing my own DOM diff

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 
 exports.create = function(initialData) {
-  
+
 }
 exports.apply = function(snapshot, op) {}
 exports.transform = function(op1, op2, side) {}
 exports.compose  = function(op1, op2) {}
 
-
-
-
+exports.Move = require('./lib/index').Move
+exports.Manipulate = require('./lib/index').Manipulate
