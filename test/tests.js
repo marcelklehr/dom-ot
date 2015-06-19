8d958343745bf24f9a912442346e589326af9f00
Marcel Klehr <mklehr@gmx.net>
2015-06-19 22:47:42 +0200

    Fix Move#transformAgainst: Don't overtake `to` if a node with the same path is removed + add tests

--- test/tests.js
+++ test/tests.js
@@ -39,6 +39,13 @@ describe('dom-ot', function() {
       move1.from.should.deep.equal([1,1])
     })
     
+    it('should transform `Move#from` correctly for node moved', function() {
+      var move1 = new domOT.Move([0,1], null)
+        , move2 = new domOT.Move([0,1], [1,1])
+      move1.transformAgainst(move2)
+      move1.from.should.deep.equal([1,1])
+    })
+    
     it('should transform `Move#to` correctly for siblings removed', function() {
       var move1 = new domOT.Move(null, [0,1])
         , move2 = new domOT.Move([0,0], null)
@@ -74,6 +81,13 @@ describe('dom-ot', function() {
       move1.to.should.deep.equal([1,0])
     })
     
+    it('should transform `Move#to` correctly for node moved', function() {
+      var move1 = new domOT.Move(null, [0,0])
+        , move2 = new domOT.Move([0,0], [1,0])
+      move1.transformAgainst(move2)
+      move1.to.should.deep.equal([0,0])
+    })
+    
     it('should transform `Manipulate#path` correctly for siblings removed', function() {
       var manipulate = new domOT.Manipulate([0,1], 'data-attrib', 'value')
         , move2 = new domOT.Move([0,0], null)
@@ -105,6 +119,13 @@ describe('dom-ot', function() {
     it('should transform `Manipulate#path` correctly for parents moved', function() {
       var manipulate = new domOT.Manipulate([0,1], 'data-attrib', 'value')
         , move2 = new domOT.Move([0], [1])
+      manipulate.transformAgainst(move2)
+      manipulate.path.should.deep.equal([1,1])
+    })
+    
+    it('should transform `Manipulate#path` correctly for node moved', function() {
+      var manipulate = new domOT.Manipulate([0,1], 'data-attrib', 'value')
+        , move2 = new domOT.Move([0,1], [1,1])
       manipulate.transformAgainst(move2)
       manipulate.path.should.deep.equal([1,1])
     })
