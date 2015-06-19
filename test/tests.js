2df7f363d062cac21772dcf71e27a1a07144ee0d
Marcel Klehr <mklehr@gmx.net>
2015-06-19 18:16:59 +0200

    Add tests for Move#transformAgainst

---
+++ test/tests.js
@@ -0,0 +1,77 @@
+var chai = require('chai')
+  , should = chai.should()
+  , domOT = require('../')
+
+describe('dom-ot', function() {
+  describe('transformations', function() {
+    it('should transform `Move#from` correctly for siblings removed', function() {
+      var move1 = new domOT.Move([0,1], null)
+        , move2 = new domOT.Move([0,0], null)
+      move1.transformAgainst(move2)
+      move1.from.should.deep.equal([0,0])
+    })
+    
+    it('should transform `Move#from` correctly for siblings removed', function() {
+      var move1 = new domOT.Move([0,0], null)
+        , move2 = new domOT.Move([0,1], null)
+      move1.transformAgainst(move2)
+      move1.from.should.deep.equal([0,0])
+    })
+    
+    it('should transform `Move#from` correctly for siblings added', function() {
+      var move1 = new domOT.Move([0,0], null)
+        , move2 = new domOT.Move(null, [0,0])
+      move1.transformAgainst(move2)
+      move1.from.should.deep.equal([0,1])
+    })
+    
+    it('should transform `Move#from` correctly for siblings added', function() {
+      var move1 = new domOT.Move([0,0], null)
+        , move2 = new domOT.Move(null, [0,1])
+      move1.transformAgainst(move2)
+      move1.from.should.deep.equal([0,0])
+    })
+    
+    it('should transform `Move#from` correctly for parents moved', function() {
+      var move1 = new domOT.Move([0,1], null)
+        , move2 = new domOT.Move([0], [1])
+      move1.transformAgainst(move2)
+      move1.from.should.deep.equal([1,1])
+    })
+    
+    it('should transform `Move#to` correctly for siblings removed', function() {
+      var move1 = new domOT.Move(null, [0,1])
+        , move2 = new domOT.Move([0,0], null)
+      move1.transformAgainst(move2)
+      move1.to.should.deep.equal([0,0])
+    })
+    
+    it('should transform `Move#to` correctly for siblings removed', function() {
+      var move1 = new domOT.Move(null, [0,1])
+        , move2 = new domOT.Move([0,2], null)
+      move1.transformAgainst(move2)
+      move1.to.should.deep.equal([0,1])
+    })
+    
+    it('should transform `Move#to` correctly for siblings added', function() {
+      var move1 = new domOT.Move(null, [0,1])
+        , move2 = new domOT.Move(null, [0,0])
+      move1.transformAgainst(move2)
+      move1.to.should.deep.equal([0,2])
+    })
+    
+    it('should transform `Move#to` correctly for siblings added', function() {
+      var move1 = new domOT.Move(null, [0,1])
+        , move2 = new domOT.Move(null, [0,2])
+      move1.transformAgainst(move2)
+      move1.to.should.deep.equal([0,1])
+    })
+    
+    it('should transform `Move#to` correctly for parents moved', function() {
+      var move1 = new domOT.Move(null, [0,0])
+        , move2 = new domOT.Move([0], [1])
+      move1.transformAgainst(move2)
+      move1.to.should.deep.equal([1,0])
+    })
+  })
+})
